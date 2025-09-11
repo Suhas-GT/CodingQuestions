af87745e8e5e0cab70d9bee12978d16c0e6e1056
Suhas G T <127110957+Suhas-GT@users.noreply.github.com>
2025-09-11 17:46:58 +0530

    Add files via upload

---
+++ Array/majorityElement.py
@@ -0,0 +1,22 @@
+# 169. Majority Element
+# https://leetcode.com/problems/majority-element/   
+
+# Given an array nums of size n, return the majority element.
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+# Example 1:
+# Input: nums = [3,2,3]
+# Output: 3
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        n=len(nums)
+        return nums[n//2]
+
+s= Solution()
+print(s.majorityElement([3,2,3]))
+print(s.majorityElement([2,2,1,1,1,2,2]))
