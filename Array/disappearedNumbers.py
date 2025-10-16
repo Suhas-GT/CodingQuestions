a70ffb1980b3becf3e87067a82f64c97cf5a908e
Suhas G T <127110957+Suhas-GT@users.noreply.github.com>
2025-10-16 13:41:09 +0530

    Add files via upload

---
+++ Array/disappearedNumbers.py
@@ -0,0 +1,27 @@
+# 448. Find All Numbers Disappeared in an Array
+# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array
+
+# Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+# Example 1:
+# Input: nums = [4,3,2,7,8,2,3,1]
+# Output: [5,6]
+# Example 2:
+# Input: nums = [1,1]
+# Output: [2]
+
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        s=set(nums)
+        res=[]
+        for i in range(1,len(nums)+1):
+            if i not in s:
+                res.append(i)
+        return res
+        
+s= Solution()
+print(s.findDisappearedNumbers([4,3,2,7,8,2,3,1]))
+print(s.findDisappearedNumbers([1,1]))
